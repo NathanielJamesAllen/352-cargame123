c9c81551d1c8c37ca01a79576acbad670f1058b9
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2023-04-28 13:50:55 -0400

    Mario PowerUps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,16 +6,23 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 60f;
     [SerializeField] float moveSpeed = 10f;
-
     [SerializeField] float boostSpeed = 40f;
     [SerializeField] float slowSpeed = 5f;
-
     float currentSpeed;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite YellowBus;
+    public Sprite BlueCar;
+    public Sprite Racer;
+    void ChangeSprite(Sprite newSprite) {
+        spriteRenderer.sprite = newSprite; 
+    }
 
     // Start is called before the first frame update
     void Start()
     {  
         currentSpeed = moveSpeed;  
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
       
     }
      
@@ -26,11 +33,53 @@ public class Driver : MonoBehaviour
         else if(other.tag == "Slower"){
             moveSpeed = slowSpeed;
         }
+         if(other.gameObject.tag == "Big Boi"){
+            gameObject.transform.localScale +=  new Vector3(1,1, 0);
+            StartCoroutine(BigBoi(7));
+            Destroy(other.gameObject, 0.2f);
+        }
+        if(other.gameObject.tag == "Lil Goy"){
+            gameObject.transform.localScale -=  new Vector3(0.2f,0.2f, 0);
+            StartCoroutine(LilGoy(7));
+            Destroy(other.gameObject, 0.2f);
+        } 
+        if(other.gameObject.tag == "Bus"){
+            ChangeSprite(YellowBus);
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector3(2.5f,7, 0);
+            StartCoroutine(Bussie(15));
+            Destroy(other.gameObject, 0.2f);
+        } 
+        if(other.gameObject.tag == "Racer"){
+            ChangeSprite(Racer);
+            StartCoroutine(RacerCar(15));
+            Destroy(other.gameObject, 0.2f);
+        } 
 
     }
      void OnTriggerExit2D(Collider2D other) {
          moveSpeed = 20;
         
+    }
+
+        IEnumerator LilGoy(float time) {
+     yield return new WaitForSeconds(time);
+        gameObject.transform.localScale +=  new Vector3(0.2f,0.2f, 0);
+    }
+
+    IEnumerator BigBoi(float time) {
+     yield return new WaitForSeconds(time);
+        gameObject.transform.localScale -=  new Vector3(1,1, 0);
+    }
+
+    IEnumerator Bussie(float time) {
+     yield return new WaitForSeconds(time);
+            ChangeSprite(BlueCar);
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector3(1.8f,3.495438f, 0);
+    }
+    
+    IEnumerator RacerCar(float time) {
+     yield return new WaitForSeconds(time);
+            ChangeSprite(BlueCar);
     }
 
     
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Instructions to the car pick up game:
 1. use the arrow keys or the WASD keys to move around.
 2. Pick up the red packages and drop them off at the green drop spot.
 3. Drive on the orange capsules for a boost and don't drive in the water.
+4. Scoreboard and timer included.
+5. Powerups: small white box = mini; large white box = large; yellow car = bus; race car = racecar.
 4. Drive past the bridge at your own risk! 
